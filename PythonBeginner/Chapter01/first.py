86c2fc6f9d2c80fd8abb2c4d13c62d67256aaa3c
GitAi-Ganesh <work.ganeshsalunke312@gmail.com>
2025-08-26 12:40:08 +0530

    Create index.py
    First Chapter

---
+++ PythonBeginner/Chapter01/first.py
@@ -0,0 +1,2 @@
+# I  am starting my first code with python
+print ("hello world")
---
+++ PythonBeginner/Chapter01/modules.py
@@ -0,0 +1,15 @@
+# Modules are the pre written code which are written by others and we can use them by importing them 
+# to import them first we have to install them on cmd etc
+# Ex : pip install Pyjokes
+import pyjokes
+jokes = (pyjokes.get_joke())
+print(jokes)   
+
+
+ # comments 
+# two types  of comments 
+""" 1) Single Line Comments 
+ using "# " for this 
+ 2) Multi Line Comments
+ using three Double inverted comas"""
+ 
